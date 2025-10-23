20a563c49d49fa50c64df7b3c1a2240621466959
Theus3ds <limasantosmatheus134@outlook.com>
2025-10-23 17:56:36 +0000

    Versão

--- guiaPraias/app/(tabs)/index.tsx
+++ guiaPraias/app/(tabs)/index.tsx
@@ -1,21 +1,76 @@
-import { Background } from '@react-navigation/elements';
 import react from 'react';
 import {Stylesheet, View, Text, Image, TouchableOpacity, SafeAreaView, StatusBar} from 'react-native';
-export function home() {
+
+const CategoryCard = ({backgroudColor, title, titleCçolor = '#fff'}) =>
+(
+<TouchabLeOpacity style={[styles.card, {backgroudColor}]}>
+  <Text style={[styles.cardText, {color: titleColor}]}></Text>
+</TouchabLeOpacity>
+);
+
+export default function home(){
   return(
-    <SafeAreaView style= {Stylesheet.container}>
-      <StatusBar barStyle="dark-content"/>
-      <Image source= {require('./assets/praia.jpg')} style={Styles.headerImage}/>
-      <View style= {Styles.titleContainer}>
-     <Text style= {Styles.mainTitle}> Guia de Praia </Text>
-     <Text style = {Styles.subTitles}> Planeje sua Viagem </Text> 
-     </View>  
-  </SafeAreaView>
-  );
+    <SafeAreaView style={styles.container}>
+      <StatusBar barstyle="dark-content"/>
+        <Image
+        source={require('../assets/praias.jpg')}
+        style={styles.headerImage}/>
+    </SafeAreaView>
+  )
 }
-const style = StyleSheet.create({
-  container:{
+
+const styles = StyleSheet.create({
+  container: {
     flex: 1,
-    BackgroundColor: '#f0f4f8',
-  alignItens:'center',},
-});
+    backgroundColor:'#fff'
+    alignItems: 'center', 
+  },
+  headerImage:{
+    width:'100%',
+    height: 250,
+    resizeMode: 'cover',
+
+   },
+   titleContainer:{
+    marginVertical: 24,
+    alignItems: 'center',
+   },
+   mainTitle: {
+    fontSize: 28,
+    fontWeight: 'bold',
+    color: '#1e63ff',
+   },
+   subTitle:{
+    fontSize: 16,
+    color: '#48d6d2',
+    marginTop: 4,
+   },
+   grid: {
+    width 90,
+
+   },
+   row: {
+    flexDirection: 'row'
+    justfyContent: 'space-between',
+    marginBottom 16,
+   },
+   card: {
+    width: '48%',
+    heigth: '120',
+    borderRadius: '20',
+    justifyContent: 'center',
+    alignItems: 'center',
+    sadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.85,
+    elevation 5,
+   },
+   cardText: {
+   fontSize: 18,
+   fontWeight: 'bold',
+   },
+})
