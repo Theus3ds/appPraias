500ea29862c32b49fce167133690e462ee8e179a
Theus3ds <limasantosmatheus134@outlook.com>
2025-11-26 18:33:37 +0000

    final

--- guiaPraias/app/(tabs)/config.tsx
+++ guiaPraias/app/(tabs)/config.tsx
@@ -82,7 +82,7 @@ setModalVisible(!isModalVisible);
 <View style={styles.modalOverlay}>
 {/* Caixa de conteúdo do modal */}
 <View style={styles.modalContent}>
-<Text style={styles.modalText}>Desenvolvido por: Aluno</Text>
+<Text style={styles.modalText}>Desenvolvido por: Matheus Lima</Text>
 <TouchableOpacity
 
 style={styles.modalCloseButton}
